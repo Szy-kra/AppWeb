a25e3694e57734a5ae273aaaebbb98e7df074233
sellavi87 <krajewski.sz@outlook.com>
2025-12-31 17:29:45 +0100

    Add project files.

---
+++ Materiały/p_jtjaskulski-08112025145100-SolucjaPoprzednia/Solucja/Moja1/Moja2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Moja2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
